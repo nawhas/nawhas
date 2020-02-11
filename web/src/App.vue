632a6dd967bc993e3714882e32e4b3be29cf167d
Zain Mehdi <szainmehdi@gmail.com>
2020-02-11 09:54:50 -0500

    Make frontend work a bit more.
    Copy the layout from the old app. TODO: Needs reworking

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <div id="app" :class="'app--' + $vuetify.breakpoint.name">
-<!--    <vue-progress-bar></vue-progress-bar>-->
+    <!--<vue-progress-bar></vue-progress-bar>-->
     <router-view></router-view>
-  </div>
 </template>
 
 <script lang="ts">
