bf01e695523972d9566b619f1c86165bdceeb5ea
Asif Ali <shea786@live.co.uk>
2020-02-12 22:43:29 +0000

    Revert "Fixed the reciters profile page to add the header of the page"
    This reverts commit 845d7b7fd827702c16e45b0ebd81e669acb0d5fe.

--- web/src/views/public/reciters/Show.vue
+++ web/src/views/public/reciters/Show.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <div class="reciter-hero">
-      <div class="reciter-hero__ribbon"></div>
-      <div class="reciter-hero__content">
-        <v-card class="reciter-hero__card">
-          <div class="reciter-hero__avatar">
-            <v-avatar size="152px" class="white">
-              <img :src="reciter.avatar" :alt="reciter.name" />
-            </v-avatar>
-          </div>
-          <h4 class="reciter-hero__title">
-            {{ reciter.name }}
-          </h4>
-          <p class="reciter-hero__bio">{{ reciter.description }}</p>
-        </v-card>
-      </div>
-    </div>
     <section class="page-section" id="top-reciters-section">
       <h5 class="title">Top Nawhas</h5>
       <v-container grid-list-lg class="pa-0" fluid>
@@ -78,57 +62,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.reciter-hero {
-  .reciter-hero__ribbon {
-    width: 100%;
-    height: 220px;
-    margin-bottom: -220px;
-    background: linear-gradient(to bottom right, #E90500, #FA6000);
-  }
-
-  .reciter-hero__content {
-    padding: 80px 120px 24px 120px;
-  }
-
-  .reciter-hero__avatar {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    top: -80px;
-    margin-bottom: -56px;
-
-    .avatar {
-      box-sizing: content-box;
-      border: 5px solid white;
-    }
-  }
-
-  .reciter-hero__card {
-    margin-top: 36px;
-    width: 100%;
-    min-height: 20px;
-    position: relative;
-    padding: 0 36px 24px 36px;
-  }
-
-  .reciter-hero__title {
-    font-family: 'Roboto Slab', sans-serif;
-    font-weight: 600;
-    color: #2e2e2e;
-    text-align: center;
-    margin: 0;
-    padding: 0;
-  }
-
-  .reciter-hero__bio {
-    margin: 16px 0 0 0;
-    padding: 0;
-    max-height: 108px;
-    overflow: hidden;
-    position: relative;
-  }
-}
 .title {
   margin-bottom: 12px;
 }
