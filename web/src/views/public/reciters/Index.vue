e55c1f985fbc7888c1eff52fbe746d1fa269382c
Asif Ali <shea786@live.co.uk>
2020-02-12 13:57:34 +0000

    changes tsconfig to ignore line-break

--- web/src/views/public/reciters/Index.vue
+++ web/src/views/public/reciters/Index.vue
@@ -5,18 +5,18 @@
       <v-container grid-list-lg class="pa-0" fluid>
         <v-layout row wrap>
           <v-flex xs12 sm6 md4 v-for="reciter in popularReciters" :key="reciter.id">
-            <reciter-card featured v-bind="reciter"/>
+            <reciter-card featured v-bind="reciter" />
           </v-flex>
         </v-layout>
       </v-container>
     </section>
 
-    <section class="page-section" id="top-reciters-section">
+    <section class="page-section" id="all-reciters-section">
       <h5 class="title">All Reciters</h5>
       <v-container grid-list-lg class="pa-0" fluid>
         <v-layout row wrap>
           <v-flex xs12 sm6 md4 v-for="reciter in reciters" :key="reciter.id">
-            <reciter-card featured v-bind="reciter"/>
+            <reciter-card v-bind="reciter" />
           </v-flex>
         </v-layout>
       </v-container>
@@ -25,20 +25,20 @@
 </template>
 
 <script lang="ts">
-import ReciterCard from "@/components/ReciterCard.vue";
-import { mapGetters } from "vuex";
-import store from "@/store";
-import { Route } from "vue-router";
+import ReciterCard from '@/components/ReciterCard.vue';
+import { mapGetters } from 'vuex';
+import store from '@/store';
+import { Route } from 'vue-router';
 
 async function fetchData() {
   await Promise.all([
-    store.dispatch("popular/fetchPopularReciters", { limit: 6 }),
-    store.dispatch("reciters/fetchReciters", { limit: 6 })
+    store.dispatch('popular/fetchPopularReciters', { limit: 6 }),
+    store.dispatch('reciters/fetchReciters', { limit: 6 }),
   ]);
 }
 
 export default {
-  name: "Reciters",
+  name: 'Reciters',
   components: { ReciterCard },
   async beforeRouteEnter(to: Route, from: Route, next: (to?: any) => void) {
     await fetchData();
@@ -51,7 +51,7 @@ export default {
   computed: {
     ...mapGetters({
       popularReciters: 'popular/popularReciters',
-      reciters: 'reciters/reciters'
+      reciters: 'reciters/reciters',
     }),
   },
 };
--- web/src/views/public/reciters/Show.vue
+++ web/src/views/public/reciters/Show.vue
@@ -1,7 +1,56 @@
 <template>
-  <div></div>
+  <div>
+    <section class="page-section" id="top-reciters-section">
+      <h5 class="title">Top Nawhas</h5>
+      <v-container grid-list-lg class="pa-0" fluid>
+        <v-layout row wrap>
+          <v-flex xs12 sm6 md4 v-for="track in popularTracks" v-bind:key="track.id">
+            <track-card v-bind="track" :show-reciter="false"></track-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { mapGetters } from 'vuex';
+import TrackCard from '@/components/TrackCard.vue';
+import store from '@/store';
+
+async function fetchData(reciter) {
+  await Promise.all([
+    store.dispatch('albums/fetchAlbums', { reciter }),
+    store.dispatch('reciters/fetchReciter', { reciter }),
+  ]);
+  await store.dispatch('popular/fetchPopularTracks', { limit: 6, reciterId: store.getters['reciters/reciter'].id });
+}
+
+export default {
+  name: 'reciters.show',
+  components: {
+    TrackCard,
+  },
+  async beforeRouteEnter(to, from, next) {
+    await fetchData(to.params.reciter);
+    next();
+  },
+  async beforeRouteUpdate(to, from, next) {
+    await fetchData(to.params.reciter);
+    next();
+  },
+  computed: {
+    ...mapGetters({
+      reciter: 'reciters/reciter',
+      albums: 'albums/albums',
+      popularTracks: 'popular/popularTracks',
+    }),
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.title {
+  margin-bottom: 12px;
+}
+</style>
