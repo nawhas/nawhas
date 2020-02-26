dd169baeaa67dea1970a1a46ccfb0d13b59270a4
Zain Mehdi <szainmehdi@gmail.com>
2020-02-25 19:22:48 -0500

    Fix https locally.

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,11 +1,18 @@
-/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/no-var-requires,@typescript-eslint/camelcase */
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
   productionSourceMap: false,
   devServer: {
-    https: true,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../docker/nginx/certs/nawhas.test.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../docker/nginx/certs/nawhas.test.crt')),
+    },
+    public: 'nawhas.test:8080',
   },
   pwa: {
     name: 'Nawhas.com',
