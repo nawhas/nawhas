211a65b86ce1f1289783c68d39153c7836e3e34f
Asif Ali <shea786@live.co.uk>
2020-02-28 01:17:16 +0000

    Added v-slider without any styling
    Co-Authored-By: Zain Mehdi <szainmehdi@gmail.com>

--- web/src/components/audio-player/AudioPlayer.vue
+++ web/src/components/audio-player/AudioPlayer.vue
@@ -3,7 +3,22 @@
       <div class="artwork">
         <img src="/img/default-album-image.png" />
       </div>
-      <div class="">
+      <div class="player-content">
+        <div class="seek-bar">
+          <v-slider
+            v-model="currentSeek"
+            :min="startTime"
+            :max="endTime"
+            hide-details>
+          </v-slider>
+          <div class="current-time">
+            {{ currentSeek }}
+          </div>
+          <v-spacer></v-spacer>
+          <div class="end-time">
+            {{ endTime }}
+          </div>
+        </div>
       </div>
     </div>
 </template>
@@ -13,11 +28,14 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class AudioPlayer extends Vue {
-
+  public currentSeek = 0;
+  public startTime = 0;
+  public endTime = 360;
 }
 </script>
 
 <style lang="scss" scoped>
+@import '~vuetify/src/styles/styles';
 
 .audio-player {
     width: 100%;
@@ -28,9 +46,20 @@ export default class AudioPlayer extends Vue {
     left: 0;
     z-index: 10;
     box-shadow: 0 -2px 8px 4px rgba(0,0,0,0.16);
+    display: flex;
+
 
     .artwork img {
       width: 80px;
+    }
+
+    .player-content {
+      width: 100%;
+    }
+}
+@media #{map-get($display-breakpoints, 'md-and-down')} {
+    .audio-player {
+      z-index: 3 !important;
     }
 }
 </style>
