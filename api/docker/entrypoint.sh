d4db5d7aebe3e1fca7ae780b3520b51c1a4e8c52
Zain Mehdi <szainmehdi@gmail.com>
2020-03-08 13:44:05 -0400

    Revert "Remove the boot command in the entrypoint."
    This reverts commit f5d7cd3707c4baa4adac5f3330421ee2fcac8495.

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 set -xe
 
-# If no command is provided (i.e. container running with default command)
-# then we want to boot the application before starting PHP and nginx
 if [ $# -eq 0 ]; then
-  # php artisan boot
+  php artisan boot
   exec docker-entrypoint
 else
-  # docker run --rm {container} php artisan migrate
   exec docker-entrypoint "$@"
 fi
