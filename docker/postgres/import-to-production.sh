8bc2026d988dfe45b6d90ccb8744f66cb62c75b7
Zain Mehdi <szainmehdi@gmail.com>
2020-03-14 14:44:35 -0400

    Import to production script.

--- docker/postgres/import-to-production.sh
+++ docker/postgres/import-to-production.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-exit 1; # DO NOT RUN THIS COMMAND
 set -ex
 
 FILENAME=$1
@@ -11,7 +10,6 @@ then
 fi
 
 DUMP="/opt/$1"
-POSTGRES_DB=nawhas_production
 
 if [ ! -f "$DUMP" ]
 then
@@ -21,15 +19,8 @@ fi
 
 echo "Importing $DUMP to production database.";
 
-#psql -U "$POSTGRES_USER" -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$POSTGRES_DB' AND pid <> pg_backend_pid();" > /dev/null
-
-psql -U "$POSTGRES_USER" -d postgres -c "DROP DATABASE $POSTGRES_DB;"
-psql -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $POSTGRES_DB;"
-
-pg_restore -d "$PROD_CONN_STR" \
+pg_restore -d "$PROD_CONNECTION" \
   --no-privileges \
-  --clean \
-  --if-exists \
   --no-acl \
   --no-owner \
   --jobs 4 \
