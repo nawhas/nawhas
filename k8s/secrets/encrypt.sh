0fd17f78ad84af5760944afec2e4f6404ee6f35c
Zain Mehdi <szainmehdi@gmail.com>
2020-03-15 12:16:45 -0400

    Include staging secrets.

--- k8s/secrets/encrypt.sh
+++ k8s/secrets/encrypt.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-set -x && \
-  gpg --symmetric --cipher-algo AES256 api.env
+set -x;
+
+gpg --symmetric --cipher-algo AES256 api.env;
+gpg --symmetric --cipher-algo AES256 api.staging.env;
