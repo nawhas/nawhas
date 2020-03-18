028f9fbc0d21f5a9e4c1f4a9de686a60841eb11c
Asif Ali <shea786@live.co.uk>
2020-03-18 12:27:00 +0000

    made the track queue current track display lighter

--- web/src/components/audio-player/QueueList.vue
+++ web/src/components/audio-player/QueueList.vue
@@ -106,7 +106,7 @@ export default class QueueList extends Vue {
   background-color: map-deep-get($colors, 'deep-orange', 'lighten-5');
 }
 .queue-item--active--dark {
-  background-color: map-deep-get($colors, 'deep-orange', 'lighten-1');
+  background-color: map-deep-get($colors, 'deep-orange', 'lighten-3');
 }
 .playback-progress {
   margin-right: 8px;
