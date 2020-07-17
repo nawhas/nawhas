0f0311e9b3b9f80e63bb886fa49a05335729a606
Zain Mehdi <szainmehdi@gmail.com>
2020-07-17 13:44:45 -0700

    Fix deploy errors.

--- api/database/migrations/doctrine/Version20200701003746.php
+++ api/database/migrations/doctrine/Version20200701003746.php
@@ -10,7 +10,7 @@ class Version20200701003746 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
@@ -27,7 +27,7 @@ class Version20200701003746 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
