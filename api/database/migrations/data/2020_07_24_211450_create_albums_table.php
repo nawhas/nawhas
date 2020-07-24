3d8bdde845c050e7d9bcd74aa2f74887a0ac2043
Asif Ali <shea786@live.co.uk>
2020-07-24 22:18:50 +0100

    Create 2020_07_24_211450_create_albums_table.php

---
+++ api/database/migrations/data/2020_07_24_211450_create_albums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('data')->create('albums', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('title');
+            $table->string('year');
+            $table->string('artwork');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
