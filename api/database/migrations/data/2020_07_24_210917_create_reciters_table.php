a9c0c3a8868d3c2d46d25b889bab0b6e8443fdd7
Asif Ali <shea786@live.co.uk>
2020-07-24 22:13:46 +0100

    Create 2020_07_24_210917_create_reciters_table.php

---
+++ api/database/migrations/data/2020_07_24_210917_create_reciters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecitersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('data')->create('reciters', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('avatar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reciters');
+    }
+}
