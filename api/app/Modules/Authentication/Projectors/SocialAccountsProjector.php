4742a3ea994d4b4b3fdbeb7317399cf537824d38
Asif Ali <shea786@live.co.uk>
2020-07-26 02:37:41 +0100

    Create SocialAccountsProjector.php

---
+++ api/app/Modules/Authentication/Projectors/SocialAccountsProjector.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Modules\Authentication\Projectors;
+
+
+use App\Modules\Authentication\Events\SocialAccountRegistered;
+use App\Modules\Authentication\Models\SocialAccount;
+use Spatie\EventSourcing\EventHandlers\Projectors\Projector;
+
+class SocialAccountsProjector extends Projector
+{
+    public function onSocialAccountRegistered(SocialAccountRegistered $event): void
+    {
+        $data = collect($event->attributes);
+        $data->put('id', $event->id);
+
+        $socialAccount = new SocialAccount($data->all());
+
+        $socialAccount->saveOrFail();
+    }
+
+    public function resetState(): void
+    {
+        SocialAccount::truncate();
+    }
+}
