fe5bdee8f779466137776cbd9ac725a4ff505978
Asif Ali <shea786@live.co.uk>
2020-07-27 02:08:41 +0100

    Created visits table

---
+++ api/database/migrations/data/2020_07_27_002743_create_visits_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('data')->create('visits', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('entity');
+            $table->uuid('entity_id');
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('data')->dropIfExists('visits');
+    }
+}
