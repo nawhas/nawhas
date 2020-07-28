1ec6e28b42e49951640175283747bcc0dfd00d69
Asif Ali <shea786@live.co.uk>
2020-07-28 22:45:44 +0100

    Removed Vists model

--- api/app/Modules/Library/Projectors/PopularEntitiesProjector.php
+++ api/app/Modules/Library/Projectors/PopularEntitiesProjector.php
@@ -6,7 +6,8 @@ namespace App\Modules\Library\Projectors;
 
 use App\Modules\Library\Events\Reciters\ReciterViewed;
 use App\Modules\Library\Events\Tracks\TrackViewed;
-use App\Modules\Library\Models\Visit;
+use App\Modules\Library\Models\Reciter;
+use App\Modules\Library\Models\Track;
 use Spatie\EventSourcing\EventHandlers\Projectors\Projector;
 
 class PopularEntitiesProjector extends Projector
@@ -14,26 +15,13 @@ class PopularEntitiesProjector extends Projector
     public function onReciterViewed(ReciterViewed $event): void
     {
         $data = collect($event->data);
-        $data->put('id', $event->id);
-        $data->put('entity', 'reciter');
-        $data->put('entity_id', $data->get('reciter_id'));
-        $data->forget('reciter_id');
-
-        $visit = new Visit($data->all());
-
-        $visit->saveOrFail();
+        $reciter = Reciter::retrieve($event->reciter_id);
     }
 
     public function onTrackViewed(TrackViewed $event): void
     {
         $data = collect($event->data);
-        $data->put('id', $event->id);
-        $data->put('entity', 'track');
-        $data->put('entity_id', $data->get('track_id'));
-        $data->forget('track_id');
 
-        $visit = new Visit($data->all());
-
-        $visit->saveOrFail();
+        $track = Track::retrieve($event->track_id);
     }
 }
