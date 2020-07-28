cf26eb59be9fd9c490735a5a6a8103bc005b79cb
Asif Ali <shea786@live.co.uk>
2020-07-28 13:57:03 +0100

    Update PopularEntitiesProjector.php

--- api/app/Modules/Library/Projectors/PopularEntitiesProjector.php
+++ api/app/Modules/Library/Projectors/PopularEntitiesProjector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Modules\Library\Projectors;
 
 use App\Modules\Library\Events\Reciters\ReciterViewed;
+use App\Modules\Library\Events\Tracks\TrackViewed;
 use App\Modules\Library\Models\Visit;
 use Spatie\EventSourcing\EventHandlers\Projectors\Projector;
 
@@ -17,6 +18,19 @@ class PopularEntitiesProjector extends Projector
         $data->put('entity', 'reciter');
         $data->put('entity_id', $data->get('reciter_id'));
         $data->forget('reciter_id');
+
+        $visit = new Visit($data->all());
+
+        $visit->saveOrFail();
+    }
+
+    public function onTrackViewed(TrackViewed $event): void
+    {
+        $data = collect($event->data);
+        $data->put('id', $event->id);
+        $data->put('entity', 'track');
+        $data->put('entity_id', $data->get('track_id'));
+        $data->forget('track_id');
 
         $visit = new Visit($data->all());
 
