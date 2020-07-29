0250d5a0f938eabcd191d170a0da0b7cb79ee67c
Zain Mehdi <szainmehdi@gmail.com>
2020-07-29 13:55:14 -0700

    Fix redeclare function error.

--- api/config/meilisearch.php
+++ api/config/meilisearch.php
@@ -1,22 +1,19 @@
 <?php
 
-function ms_index(string $name): string
-{
-    return env('SCOUT_PREFIX', '') . $name;
-}
+$index = fn(string $name): string => env('SCOUT_PREFIX', '') . $name;
 
 return [
     'host' => env('MEILISEARCH_HOST', 'nawhas_search:7700'),
     'key' => env('MEILISEARCH_KEY', 'secret'),
 
     'indices' => [
-        ms_index('reciters') => [
+        $index('reciters') => [
             'model' => App\Modules\Library\Models\Reciter::class,
         ],
-        ms_index('albums') => [
+        $index('albums') => [
             'model' => App\Modules\Library\Models\Album::class,
         ],
-        ms_index('tracks') => [
+        $index('tracks') => [
             'model' => App\Modules\Library\Models\Track::class,
         ],
     ],
