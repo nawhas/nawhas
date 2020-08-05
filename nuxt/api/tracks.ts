05bcaee22deae53844821602820ab5e93d3ce395
Asif Ali <shea786@live.co.uk>
2020-08-06 00:14:41 +0100

    Removed Pagination options from trakcs page

--- nuxt/api/tracks.ts
+++ nuxt/api/tracks.ts
@@ -1,15 +1,12 @@
 import { NuxtAxiosInstance } from '@nuxtjs/axios';
 import {
-  PaginationOptions,
   PersistedEntity,
   TimestampedEntity,
-  PaginatedResponse,
   createParams,
-  usePagination,
   useIncludes,
 } from '@/api/common';
-import {Reciter, RecitersIndexResponse} from '@/api/reciters';
-import {Album} from "~/api/albums";
+import { Reciter } from '@/api/reciters';
+import { Album } from '@/api/albums';
 
 /*
  * Entity Definitions
@@ -40,13 +37,12 @@ interface GetRequestOptions {
 }
 interface IndexRequestOptions {
   include?: Array<TrackIncludes>;
-  pagination?: PaginationOptions;
 }
 
 /*
  * Response Definitions
  */
-export interface TracksIndexResponse extends PaginatedResponse<Album> {}
+export interface TracksIndexResponse {}
 
 export class TracksApi {
   constructor(
@@ -63,7 +59,6 @@ export class TracksApi {
 
   async index(reciterId: string, albumId: string, options: IndexRequestOptions): Promise<TracksIndexResponse> {
     const params = createParams();
-    usePagination(params, options.pagination);
     useIncludes(params, options.include);
 
     return await this.axios.$get<TracksIndexResponse>(
@@ -74,7 +69,6 @@ export class TracksApi {
 
   async popular(options: IndexRequestOptions): Promise<TracksIndexResponse> {
     const params = createParams();
-    usePagination(params, options.pagination);
     useIncludes(params, options.include);
 
     return await this.axios.$get<TracksIndexResponse>(
