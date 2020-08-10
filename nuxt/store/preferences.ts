2192d99760c3961700b863674934e8a02d803f44
Zain Mehdi <szainmehdi@gmail.com>
2020-08-10 10:12:21 -0400

    Handle theme setting on server side

--- nuxt/store/preferences.ts
+++ nuxt/store/preferences.ts
@@ -1,12 +1,9 @@
-import { ActionTree, MutationTree } from 'vuex';
-import { RootState } from '@/store';
-
-export type Theme = 'light'|'dark'|'auto';
+import { MutationTree } from 'vuex';
+import { Theme } from '@/entities/theme';
 
 interface PreferencesState {
   theme: Theme;
 }
-const THEME_STORAGE_KEY = 'preferences.theme';
 
 const state = (): PreferencesState => ({
   theme: 'auto',
@@ -15,20 +12,11 @@ const state = (): PreferencesState => ({
 const mutations: MutationTree<PreferencesState> = {
   SET_THEME(state: PreferencesState, theme: Theme) {
     state.theme = theme;
-    localStorage.setItem(THEME_STORAGE_KEY, theme);
-  },
-};
-
-const actions: ActionTree<PreferencesState, RootState> = {
-  initialize({ commit }) {
-    const theme = localStorage.getItem(THEME_STORAGE_KEY) ?? 'auto';
-    commit('SET_THEME', theme);
   },
 };
 
 export default {
   state,
   mutations,
-  actions,
   namespaced: true,
 };
