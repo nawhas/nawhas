7b06327c127499f48d33eebed05abf55cce7b913
Asif Ali <shea786@live.co.uk>
2020-08-16 01:24:43 +0100

    Stubbed out tracks section of Library Controller

---
+++ api/app/Modules/Library/Http/Controllers/LibraryController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Library\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class LibraryController extends Controller
+{
+    public function tracks()
+    {
+
+    }
+
+    public function addTrack()
+    {
+
+    }
+
+    public function removeTrack()
+    {
+
+    }
+}
