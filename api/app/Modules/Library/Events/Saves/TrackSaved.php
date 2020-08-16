6d8b8674e014a35b2e98c59da1d145b7b41078f8
Asif Ali <shea786@live.co.uk>
2020-08-16 01:17:36 +0100

    Added events for track saved and remove saved track

---
+++ api/app/Modules/Library/Events/Saves/TrackSaved.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Library\Events\Saves;
+
+use App\Modules\Core\Events\HasUserContext;
+use App\Modules\Library\Events\UserAction;
+
+class TrackSaved extends UserAction
+{
+    use HasUserContext;
+
+    public string $id;
+    public string $trackId;
+
+    public function __construct(string $id, string $trackId)
+    {
+        $this->id = $id;
+        $this->trackId = $trackId;
+    }
+}
