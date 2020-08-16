683372adac4b496c205146f4702df88727a784a7
Asif Ali <shea786@live.co.uk>
2020-08-16 01:56:55 +0100

    Removed HasUserContext and $id

--- api/app/Modules/Library/Events/Saves/SavedTrackRemoved.php
+++ api/app/Modules/Library/Events/Saves/SavedTrackRemoved.php
@@ -4,19 +4,14 @@ declare(strict_types=1);
 
 namespace App\Modules\Library\Events\Saves;
 
-use App\Modules\Core\Events\HasUserContext;
 use App\Modules\Library\Events\UserAction;
 
 class SavedTrackRemoved extends UserAction
 {
-    use HasUserContext;
-
-    public string $id;
     public string $trackId;
 
-    public function __construct(string $id, string $trackId)
+    public function __construct(string $trackId)
     {
-        $this->id = $id;
         $this->trackId = $trackId;
     }
 }
