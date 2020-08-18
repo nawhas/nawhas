f3eaf83ea7fffbffcf4fe26f9856fbc35313cdb0
Asif Ali <shea786@live.co.uk>
2020-08-18 13:45:45 +0100

    Added PageHeader component as well as started to wrok on HomePage layout for Library

---
+++ nuxt/components/PageHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <header :class="{ 'header': true, 'header--dark': $vuetify.theme.dark }">
+      <v-container class="app__section">
+        <h1 class="header__title">
+          <slot />
+        </h1>
+      </v-container>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+
+});
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  background-color: rgba(255, 21, 0, 0.1);
+  margin-bottom: 48px;
+  position: relative;
+}
+.header--dark {
+  background: linear-gradient(30deg, #340808, #1a0000);
+}
+.header .app__section {
+  padding: 96px !important;
+}
+.header__title {
+  font-family: Roboto, sans-serif;
+  font-weight: 200;
+  font-size: 64px;
+  line-height: 75px;
+  text-align: center;
+  letter-spacing: -1.5px;
+}
+</style>
---
+++ nuxt/pages/library/HomePage.vue
@@ -0,0 +1,30 @@
+<router>
+  path: /library/home
+  name: "LibraryHomePage"
+</router>
+
+<template>
+  <div>
+    <page-header>
+      <v-icon size="64">
+        local_library
+      </v-icon>
+      Library
+    </page-header>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import PageHeader from '@/components/PageHeader';
+
+export default Vue.extend({
+  components: {
+    PageHeader,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
