9c704198483297da79f0bc89bfb5b92b0e496611
Zain Mehdi <szainmehdi@gmail.com>
2020-08-18 19:22:42 -0400

    Switch to DigitalOcean spaces as S3 replacement.

--- api/config/filesystems.php
+++ api/config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 's3'),
+    'default' => env('FILESYSTEM_DRIVER', 'spaces'),
 
     /*
     |--------------------------------------------------------------------------
@@ -71,6 +71,16 @@ return [
             'region' => 'us-east-2',
             'bucket' => 'import.dev.nawhas',
             'url' => env('AWS_URL'),
+        ],
+
+        'spaces' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'url' => env('DO_SPACES_CDN_URL'),
         ],
 
     ],
