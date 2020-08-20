8d060c81e31503e2faf99b4a692faba7567dd962
Zain Mehdi <szainmehdi@gmail.com>
2020-08-20 14:13:22 -0400

    Refresh CSRF cookie when request fails with 419.

--- nuxt/plugins/axios.ts
+++ nuxt/plugins/axios.ts
@@ -1,7 +1,20 @@
 import https from 'https';
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
 
 export default function ({ $axios, $config }) {
   if ($config.ignoreSslErrors) {
     $axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
   }
+
+  const onAxiosRequestFailure = async (failure) => {
+    if (failure.response.status === 419) {
+      await $axios.get('/sanctum/csrf-cookie');
+    }
+
+    return Promise.resolve();
+  };
+
+  createAuthRefreshInterceptor($axios, onAxiosRequestFailure, {
+    statusCodes: [419],
+  });
 }
