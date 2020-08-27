bb8ff91d903aae41c338456cfd8215c62bfd1cd1
Asif Ali <shea786@live.co.uk>
2020-08-27 01:43:21 +0100

    Stated to add the Library Store

---
+++ nuxt/store/library.ts
@@ -0,0 +1,29 @@
+import { ActionTree, MutationTree } from 'vuex';
+import { RootState } from '@/store/index';
+
+interface libraryState {
+  tracks: Array<any>;
+}
+
+const state = (): libraryState => ({
+  tracks: [],
+});
+
+const mutations: MutationTree<libraryState> = {
+  SET_TRACKS(state, data) {
+    state.tracks = data.data;
+  },
+};
+
+const actions: ActionTree<libraryState, RootState> = {
+  async getTracks({ commit }) {
+    const response = await this.$api.library.tracks;
+    commit('SET_TRACKS', response);
+  },
+};
+
+export default {
+  state,
+  actions,
+  mutations,
+};
