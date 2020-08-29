86f67b16a519991778dc2ea6532d213ff66e7025
Zain Mehdi <szainmehdi@gmail.com>
2020-08-29 13:57:30 -0400

    Code cleanup.

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -41,7 +41,6 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             // 'throttle:60,1', // TODO - Make this work with proxies.
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Modules\Library\Http\Middleware\FlushRepository::class,
             \Spatie\ResponseCache\Middlewares\CacheResponse::class,
         ],
     ];
--- api/app/Modules/Library/ServiceProvider.php
+++ api/app/Modules/Library/ServiceProvider.php
@@ -4,17 +4,11 @@ declare(strict_types=1);
 
 namespace App\Modules\Library;
 
-use App\Modules\Library\Repositories\LibraryAggregateRepository;
 use Illuminate\Support\Facades\Route as Router;
 use Illuminate\Support\ServiceProvider as CoreServiceProvider;
 
 class ServiceProvider extends CoreServiceProvider
 {
-    public function register()
-    {
-        $this->app->singleton(LibraryAggregateRepository::class);
-    }
-
     public function boot(): void
     {
         $this->defineRoutes();
