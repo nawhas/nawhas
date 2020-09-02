8f32cf3ef94a47f4d344386fb9d57d33baa0dcbb
Asif Ali <shea786@live.co.uk>
2020-09-02 10:09:26 +0100

    Removed sortying

--- nuxt/api/library.ts
+++ nuxt/api/library.ts
@@ -4,7 +4,7 @@ import {
   PaginatedResponse,
   createParams,
   usePagination,
-  useIncludes, useSortDir, useSortBy,
+  useIncludes,
 } from '@/api/common';
 import { Track } from '@/entities/track';
 import { TrackIncludes, TracksIndexResponse } from '@/api/tracks';
@@ -22,8 +22,6 @@ export interface TracksPayload {
 interface IndexRequestOptions {
   include?: Array<TrackIncludes|string>;
   pagination?: PaginationOptions;
-  sortBy?: string;
-  sortDir?: 'asc'|'desc';
 }
 
 /*
@@ -40,8 +38,6 @@ export class LibraryApi {
     const params = createParams();
     usePagination(params, options.pagination);
     useIncludes(params, options.include);
-    useSortDir(params, options.sortDir);
-    useSortBy(params, options.sortBy);
 
     return await this.axios.$get<TracksIndexResponse>('/v1/me/tracks', { params });
   }
