a8049000360dae93135271cb9f4525a38f8e76b6
Zain Mehdi <szainmehdi@gmail.com>
2020-10-29 09:59:19 -0400

    Fix linting errors.

--- api/app/Providers/EventServiceProvider.php
+++ api/app/Providers/EventServiceProvider.php
@@ -18,9 +18,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-            '\SocialiteProviders\\Apple\\AppleExtendSocialite@handle',
-        ],
     ];
 
     /**
