3ede1c4f74d50828befa59efc315707b84bd53d3
Asif Ali <shea786@live.co.uk>
2021-06-17 17:20:13 +0100

    Merged in changed made in other branch

---
+++ nuxt/pages/browse/BrowsePage.vue
@@ -0,0 +1,74 @@
+<router>
+path: /browse
+name: "browse.index"
+</router>
+
+<template>
+  <div>
+    <browse-by-card
+      browse-by="reciter"
+      :popular-reciters="popularReciters"
+    />
+    <browse-by-card
+      browse-by="topic"
+      :topics="popularTopics"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BrowseByCard from '@/components/BrowseByCard.vue';
+import { ReciterIncludes } from '@/api/reciters';
+import { Reciter } from '@/entities/reciter';
+import { Topic } from '@/entities/topic';
+import { generateMeta } from '@/utils/meta';
+import { TopicIncludes } from '@/api/topics';
+
+interface Data {
+  popularReciters: Array<Reciter> | null;
+  popularTopics: Array<Topic> | null;
+}
+
+export default Vue.extend({
+  components: {
+    BrowseByCard,
+  },
+
+  async fetch() {
+    const [popularReciters, popularTopics] = await Promise.all([
+      this.$api.reciters.popular({
+        include: [ReciterIncludes.Related],
+        pagination: { limit: 6 },
+      }),
+      // TODO - make this a popular topics route
+      this.$api.topics.index({
+        include: [TopicIncludes.Related],
+        pagination: { limit: 6 },
+      }),
+    ]);
+
+    this.popularReciters = popularReciters.data;
+    this.popularTopics = popularTopics.data;
+  },
+
+  data: (): Data => ({
+    popularReciters: null,
+    popularTopics: null,
+  }),
+
+  head: () => generateMeta({
+    title: 'Browse',
+    // TODO - Add description
+  }),
+});
+</script>
+
+<style lang="scss" scoped>
+.view-wrapper {
+  padding-top: 24px;
+}
+.pagination {
+  padding: 24px;
+}
+</style>
