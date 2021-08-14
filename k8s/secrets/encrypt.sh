f0bff8b51b3b31c6fe6aaa1663c3d0b6c2f0bbe3
Zain Mehdi <szainmehdi@gmail.com>
2021-08-14 09:55:33 -0700

    Use http for search service internal communication.

--- k8s/secrets/encrypt.sh
+++ k8s/secrets/encrypt.sh
@@ -2,5 +2,6 @@
 
 set -x;
 
-gpg --symmetric --cipher-algo AES256 api.env;
-gpg --symmetric --cipher-algo AES256 api.staging.env;
+gpg --symmetric --cipher-algo AES256 ../overlays/production/secrets/api.env;
+gpg --symmetric --cipher-algo AES256 ../overlays/staging/secrets/api.staging.env;
+gpg --symmetric --cipher-algo AES256 ../overlays/integration/secrets/api.integration.env;
