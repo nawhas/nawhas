6ba197ae99fb4f5525547ad8146069c3263ef4cc
Asif Ali <shea786@live.co.uk>
2023-08-03 23:00:01 +0100

    Removed Telescope Service Provider from app config

--- api/config/app.php
+++ api/config/app.php
@@ -171,7 +171,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
 
         /*
          * Application Module Service Providers
