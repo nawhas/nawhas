315fa4183a18b02dd3dc4022d2f3108cc1b1ec35
Asif Ali <shea786@live.co.uk>
2023-09-09 23:02:31 +0100

    added tests for contributor as well as incorrect password

--- api/tests/Browser/LoginTest.php
+++ api/tests/Browser/LoginTest.php
@@ -15,7 +15,7 @@ class LoginTest extends DuskTestCase
     /**
      * @test
      */
-    public function it_allows_user_to_log_in(): void
+    public function it_allows_moderator_user_to_log_in(): void
     {
         $user = $this->getUserFactory()->moderator([
             'email' => 'moderator@nawhas.com',
@@ -39,6 +39,61 @@ class LoginTest extends DuskTestCase
                     ->waitFor('@user-menu__name')
                     ->assertSeeIn('@user-menu__name', $user->name)
                     ->assertSeeIn('@user-menu__email', $user->email);
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function it_allows_contributor_user_to_log_in(): void
+    {
+        $user = $this->getUserFactory()->contributor([
+            'email' => 'contributor@nawhas.com',
+            'password' => 'secret123',
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/')
+                ->waitFor('@user-menu__avatar', seconds: 10)
+                ->click('@user-menu__avatar')
+                ->waitFor('@user-menu__login-button')
+                ->click('@user-menu__login-button')
+                ->waitFor('.auth-dialog')
+                ->assertSee('Welcome back!')
+                ->type('.auth-dialog input[type=email]', 'contributor@nawhas.com')
+                ->type('.auth-dialog input[type=password]', 'secret123')
+                ->press('.auth-dialog button[type=submit]')
+                ->waitUntilVue('authenticated', "true", '@user-menu')
+                ->pause(2000)
+                ->click('@user-menu__avatar')
+                ->waitFor('@user-menu__name')
+                ->assertSeeIn('@user-menu__name', $user->name)
+                ->assertSeeIn('@user-menu__email', $user->email);
+        });
+    }
+
+    // test to ensure incorrect password does not allow user to log in
+    /**
+     * @test
+     */
+    public function it_does_not_allow_incorrect_password_to_log_in(): void {
+        $user = $this->getUserFactory()->contributor([
+            'email' => 'contributor@nawhas.com',
+            'password' => 'secret123',
+        ]);
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/')
+                    ->waitFor('@user-menu__avatar', seconds: 10)
+                    ->click('@user-menu__avatar')
+                    ->waitFor('@user-menu__login-button')
+                    ->click('@user-menu__login-button')
+                    ->waitFor('.auth-dialog')
+                    ->assertSee('Welcome back!')
+                    ->type('.auth-dialog input[type=email]', 'contributor@nawhas.com')
+                    ->type('.auth-dialog input[type=password]', 'secret1234')
+                    ->press('.auth-dialog button[type=submit]')
+                    ->waitFor('.auth-dialog div[role=alert] div[class=v-alert__content]')
+                    ->assertSeeIn('.auth-dialog div[role=alert] div[class=v-alert__content]', 'These credentials do not match our records.');
         });
     }
 }
